c38d15a20c386868e3bfbe42b9d801530850ad99
zontee <whdlsxo123@naver.com>
2022-02-09 16:48:33 +0900

    02-08 예매취소 기능 구현

---
+++ src/main/java/com/oracle/HomeTheater/controller/It_RestController.java
@@ -0,0 +1,48 @@
+package com.oracle.HomeTheater.controller;
+
+
+import com.oracle.HomeTheater.model.SeatandTime;
+import com.oracle.HomeTheater.service.IT_Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class It_RestController {
+
+    private final IT_Service ITService;
+
+    @PostMapping("Cancel")
+    @ResponseBody
+    public String finalReservation(SeatandTime seatandTime){
+        String result;
+        String re_number = seatandTime.getRe_number();
+        seatandTime.setSe_identify("F");
+        //예약정보 삭제
+        log.info("deleteReservation(controller) start");
+        int deleteReservation = ITService.deleteReservation(re_number);
+        log.info("result deleteReservation =" +deleteReservation );
+        log.info("find reservation");
+        log.info("seatandTime = " + seatandTime);
+        //좌석정보 업데이트
+        log.info("SeatandTimeUpdate(controller) start");
+        int resultUpdateSeat = ITService.SeatandTimeUpdate(seatandTime);
+        log.info("updatenumber" + resultUpdateSeat);
+        //좌석정보와 예약정보 둘다 성공시 result 반환
+        if(deleteReservation ==1 && resultUpdateSeat==1){
+            result = "1";
+            return result;
+        }else {
+            result = "2";
+            return result;
+        }
+
+    }
+}
